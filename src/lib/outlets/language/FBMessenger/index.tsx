1e4074c3a8d95b1e7624c96f6a2dc426b97bad23
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-03-27 00:54:02 +0400

    fix

--- src/lib/outlets/language/FBMessenger/index.tsx
+++ src/lib/outlets/language/FBMessenger/index.tsx
@@ -1,15 +1,12 @@
 "use client"
 
 import { FacebookProvider, CustomChat } from 'react-facebook';
-import { LiveChatLoaderProvider } from 'react-live-chat-loader'
 
 const FBMessenger = () => {
     return (
-        <LiveChatLoaderProvider providerKey="766900155396947" provider="helpScout">
-            <FacebookProvider appId="766900155396947" chatSupport>
-                <CustomChat pageId="108431271487600" />
-            </FacebookProvider>
-        </LiveChatLoaderProvider>
+        <FacebookProvider appId="766900155396947" chatSupport>
+            <CustomChat pageId="108431271487600" minimized={true} />
+        </FacebookProvider>
     );
 }
 
