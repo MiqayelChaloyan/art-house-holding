4a9541777eddfe123c420d0789f1ff5aee1b8bc1
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-04-26 00:52:10 +0400

    fix arrow

--- src/components/screens/educational-center/home/News/Course/index.tsx
+++ src/components/screens/educational-center/home/News/Course/index.tsx
@@ -56,32 +56,42 @@ const Course = ({ course }: any) => {
         <div className={styles.left}>
             <h2 className={cn(styles.subtitle, Inter.className)}>{course.subtitle}</h2>
             <Content content={course.content} isReadMore={isReadMore} minimumHeight={minimumHeight} />
-           <div className={styles.group}>
-           <div className={styles['btn-group']}>
+            <div className={styles.group}>
+                <div className={styles['btn-group']}>
+                    <Button
+                        text={isReadMore ? t('buttons.view-more') : t('buttons.show-less')}
+                        className={cn(styles.button, styles['view-more-btn'], Arial.className)}
+                        onClick={toggleReadMore}
+                    />
+                    <Button
+                        text={t('buttons.contact-us')}
+                        className={cn(styles.button, styles['contact-us-btn'], Arial.className)}
+                        onClick={scrollToElement}
+                    />
+                </div>
                 <Button
-                    text={isReadMore ? t('buttons.view-more') : t('buttons.show-less')}
-                    className={cn(styles.button, styles['view-more-btn'], Arial.className)}
-                    onClick={toggleReadMore}
-                />
-                <Button
-                    text={t('buttons.contact-us')}
-                    className={cn(styles.button, styles['contact-us-btn'], Arial.className)}
-                    onClick={scrollToElement}
-                />
-            </div>
-            <div className={styles['button-more']}>
-                <Button
-                    text={t('texts.more')}
-                    className={cn(styles.link, Arial.className)}
-                    onClick={getResources}
-                />
-            </div>
-            <Button
                     text={t('texts.more')}
                     className={cn(styles['more-btn-mobile'], Arial.className)}
                     onClick={getResources}
                 />
-           </div>
+
+                <li className={styles.arrow}>
+                    <a className={styles['animated-arrow']} onClick={getResources}>
+                        <span className={cn(styles['the-arrow'], styles['-left'])}>
+                            <span className={styles.shaft}></span>
+                        </span>
+                        <span className={styles.main}>
+                            <span className={cn(styles.text, Arial.className)}>
+                                {t('texts.more')}
+                            </span>
+                            <span className={cn(styles['the-arrow'], styles['-right'])}>
+                                <span className={styles.shaft}></span>
+                            </span>
+                        </span>
+                    </a>
+                </li>
+
+            </div>
         </div>
     )
 };
