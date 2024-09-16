647bec48659fcf459e330a41751d9dcc6333c1d9
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-09-16 04:09:20 +0400

    add admin icon

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,6 +5,8 @@ import { visionTool } from '@sanity/vision';
 import { apiVersion, dataset, projectId, title, basePath } from './sanity/env';
 import { dashboardTool, projectUsersWidget, projectInfoWidget } from '@sanity/dashboard';
 
+import { BsDatabaseFillGear } from 'react-icons/bs';
+
 import Structure from './sanity.structure';
 
 
@@ -16,6 +18,7 @@ const config = defineConfig(
     apiVersion,
     basePath,
     schema,
+    icon: BsDatabaseFillGear,
     plugins: [
       deskTool({
         structure: Structure,
