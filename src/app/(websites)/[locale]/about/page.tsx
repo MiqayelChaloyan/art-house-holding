43d1dfd04940ee141540584356bd0991ed2df1f5
MiqayelChaloyan <miqochaloyan@gmail.com>
2024-09-24 05:17:41 +0400

    fix

--- src/app/(websites)/[locale]/about/page.tsx
+++ src/app/(websites)/[locale]/about/page.tsx
@@ -60,7 +60,8 @@ export async function generateMetadata({
   const { ogDescription, ogTitle, ogImage } = meta;
   const path: ImagePath = urlForImage(ogImage);
   const icon = null;
+  const keywords = null;
 
-  const metadata = generateMetadataDynamic(ogDescription, ogTitle, path, icon, locale);
+  const metadata = generateMetadataDynamic(ogDescription, ogTitle, path, icon, keywords, locale);
   return metadata;
 };
